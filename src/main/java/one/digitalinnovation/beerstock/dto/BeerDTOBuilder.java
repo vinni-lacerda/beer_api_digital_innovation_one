8d51b8d7f8eae5c3ad2ffc6041449e310b8aa887
vinni-lacerda <vini.lacerda@hotmail.com>
2025-11-30 23:08:43 -0300

    feat: creating a DTO builder to help tests

---
+++ src/main/java/one/digitalinnovation/beerstock/dto/BeerDTOBuilder.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.beerstock.builder;
+
+import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.enums.BeerType;
+
+public class BeerDTOBuilder {
+    private static final long ID = 1L;
+    private static final String NAME = "Heineken";
+    private static final int QUANTITY = 10;
+    private static final int MAX = 50;
+    private static final String BRAND = "Heineken";
+    private static final String TYPE = "ALE";
+
+    private BeerDTO beerDTO;
+
+    public BeerDTOBuilder() {
+        beerDTO = new BeerDTO();
+        beerDTO.setId(ID);
+        beerDTO.setName(NAME);
+        beerDTO.setQuantity(QUANTITY);
+        beerDTO.setMax(MAX);
+        beerDTO.setBrand(BRAND);
+        beerDTO.setType(BeerType.valueOf(TYPE));
+    }
+
+    public static BeerDTOBuilder builder() {
+        return new BeerDTOBuilder();
+    }
+
+    public BeerDTO buildBeerDTO() {
+        return this.beerDTO;
+    }
+}
